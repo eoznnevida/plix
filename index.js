c63771786993c39e81df1dd1b1212e53bfe9c3a7
eoznnevida <hrxzsegundo@gmail.com>
2026-02-03 21:33:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // =====================
 // ENV / DEPENDÊNCIAS
 // =====================
-require("dotenv").config();
+require("dotenv").config()
 const express = require("express");
 const path = require("path");
 
